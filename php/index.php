5c056a508894947b3fa071cc8d95aa1301768b9d
a24sergiobc <a24sergiobc@iessanclemente.net>
2025-02-18 14:34:41 +0100

    initial commit

---
+++ php/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title><?php echo "Sitio Web #1 en ${_SERVER['SERVER_ADDR']}"?></title>
+</head>
+<body>
+<h1>Sitio Web #1</h1>
+
+<?php echo "Est&aacute;s en ${_SERVER['SERVER_ADDR']}" ?>
+<br />
+<?php echo "Enderezo IP do cliente ${_SERVER['REMOTE_ADDR']}"?>
+</body>
+</html>
